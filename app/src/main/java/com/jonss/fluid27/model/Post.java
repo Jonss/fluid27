760d4082d9514ce8711a5fc9f4c633177c5f1ad0
Jonss <joaosantana.ti@gmail.com>
2015-08-30 02:14:04 -0300

    Criando modelo -- provisorio, preciso de mais campos

---
+++ app/src/main/java/com/jonss/fluid27/model/Post.java
@@ -0,0 +1,25 @@
+package com.jonss.fluid27.model;
+
+/**
+ * Created by neuromancer on 30/08/15.
+ */
+public class Post {
+    private String name;
+    private String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
